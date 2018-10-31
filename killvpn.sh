1a5fb1f56b7973ad7e45bf026818a01fd5a29654
Chris Baker <bakercw@verizon.net>
2018-10-31 13:43:20 -0400

    Update killvpn.sh
    Removing leading spaces from process listing makes it more resilient to changes in the output formatting

--- killvpn.sh
+++ killvpn.sh
@@ -3,9 +3,10 @@
 # Check that OpenVPN is actually running.
 running=$(ps -e | grep openvpn)
 if [ $? -eq 1 ]; then
-    echo "No active VPN session found."
+    # echo "No active VPN session found."
     exit 1
 fi
 
-pid=$(ps -e | grep openvpn | cut -d" " -f 1)
+# get process list, remove leading and trailing spaces, look for vpn, and cut out the pid
+pid=$(ps -e | sed 's/^[ \t]*//;s/[ \t]*$//' | grep openvpn | cut -d" " -f 1)
 kill $pid
